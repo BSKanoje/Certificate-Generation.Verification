fa12cbdbbe6e8cf73090886d116515c1ba4d98f8
Shreesha41223 <121144514+Shreesha41223@users.noreply.github.com>
2025-05-24 22:19:13 +0530

    Included 'logo' and 'address' feilds to Company model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import FileExtensionValidator # Import for file validation
 
 class CompanyManager(BaseUserManager):
     def create_user(self, email, company_name, phone, password=None):
@@ -25,6 +26,16 @@ class Company(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
+
+    # --- New fields for Section 8
+    logo = models.ImageField(
+        upload_to='company_logos/',
+        null=True,
+        blank=True,
+        help_text="Company logo (PNG, JPG, SVG)",
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'svg'])]
+    )
+    address = models.TextField(blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['company_name', 'phone']
